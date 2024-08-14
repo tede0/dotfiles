387d1c00c97a41533827bd6d11660b492356f0fe
Maksym Lyskov <maksym@lineupnow.com>
2024-08-14 16:50:48 +0100

    disables ligatures wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,22 +5,22 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
 
-config.color_scheme = 'GruvboxDarkHard'
+config.color_scheme = "GruvboxDarkHard"
 config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", scale = 1.2 })
 config.font_size = 14
-
+config.harfbuzz_features = { "calt=0" }
 config.enable_tab_bar = false
 config.default_prog = { "/usr/local/bin/tmux" }
 config.window_decorations = "RESIZE"
 config.keys = {
-  -- activate copy mode or vim mode
-  {
-    key = 'Enter',
-    mods = 'LEADER',
-    action = wezterm.action.ActivateCopyMode
-  },
+	-- activate copy mode or vim mode
+	{
+		key = "Enter",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
 }
 
 -- and finally, return the configuration to wezterm
